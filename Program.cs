a92ad853d510c9170e1ae22509f4d882671af6bb
IckyTheBiggy <elcia116eb@gmail.com>
2025-06-13 12:16:22 +0100

    Basic Avalonia Window

--- Program.cs
+++ Program.cs
@@ -1,23 +1,56 @@
-﻿using OpenTK.Mathematics;
-using OpenTK.Windowing.Desktop;
-
-using Rendering.UI;
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
 
 namespace Program
 {
     public class Program
     {
-        static NativeWindowSettings nativeWindowSettings = new NativeWindowSettings
-        {
-            ClientSize = new Vector2i(800, 600),
-            Title = "Khoostic"
-        };
 
         public static void Main(string[] args)
         {
-            KhoosticWindow window = new KhoosticWindow(GameWindowSettings.Default, nativeWindowSettings);
-            window.MinimumSize = new Vector2i(600, 350);
-            window.Run();
+            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        }
+
+        public static AppBuilder BuildAvaloniaApp()
+        {
+            return AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .LogToTrace();
+        }
+    }
+
+    public class App : Application
+    {
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow = new MainWindow();
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            Title = "Khoostic";
+            Width = 800;
+            Height = 600;
+
+            var button = new Button
+            {
+                Content = "Play",
+                HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Center,
+                VerticalAlignment = Avalonia.Layout.VerticalAlignment.Center
+            };
+
+            button.Click += (_, _) => button.Content = "Playing";
+
+            Content = button;
         }
     }
 }
